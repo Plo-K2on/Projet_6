aafc22b0788c8a143c3cb8332214f0547c2443b7
Hugo CF <hcaffinpgmail.com>
2022-09-05 12:30:14 +0200

    setup commit

---
+++ backend/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const dotenv = require("dotenv")
+dotenv.config();
+
+// const userRoutes = require('./routes/user');
+
+mongoose.connect(process.env.BDD_URL,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.use(bodyParser.json());
+
+// app.use('/api/auth', userRoutes);
+// app.use('/images', express.static(path.join(__dirname, 'images')));
+
+module.exports = app;
+
