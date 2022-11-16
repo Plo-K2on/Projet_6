d283e530bba6e9a5f5ab4e482398662e040707b7
Hugo CF <hcaffinpgmail.com>
2022-11-16 23:51:57 +0100

    16/11/22

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -10,8 +10,7 @@ router.post('/', auth, multer, sauceCtrl.createSauce);
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 router.get('/:id', auth, multer, sauceCtrl.getOneSauce);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
-// créer route pour modifier une sauce en fonction d'un ID
-// créer route pour avoir une sauce en fonction d'un ID
+router.post('/:id', auth, multer, sauceCtrl, likeSauce);
 router.get('/', sauceCtrl.allSauces);
 
 module.exports = router;
