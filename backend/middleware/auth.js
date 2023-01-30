56fef8ed5aa26a6c2c68458549157c37bb5d25c3
Hugo CF <hcaffinpgmail.com>
2023-01-30 11:37:23 +0100

    30/01/23

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,7 +3,6 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
    try {
        const token = req.headers.authorization.split(' ')[1];
-    //    const decodedUserId = decodedToken.userId;
        const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
        const userId = decodedToken.userId;
        req.auth = {
